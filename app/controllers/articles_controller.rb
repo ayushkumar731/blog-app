98404924e6d175f59e3eb4737db376253d905af7
Ayush Kumar <kumarayush731@gmail.com>
2020-11-02 19:43:53 +0530

    uploaded image done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,6 +54,6 @@ class ArticlesController < ApplicationController
     
     private
         def article_params
-            params.require(:article).permit(:title, :text)
+            params.require(:article).permit(:title, :text, :image)
         end
 end
